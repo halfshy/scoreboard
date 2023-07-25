3e1b41223403d004c47c93efd6d9014dd5d5227d
halfshy <61529904+halfshy@users.noreply.github.com>
2023-07-24 22:11:50 -0700

    Update index.js clean up repetition in code
    changed document.getelementbyId("") repetition within the functions to live globally in a variable.

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 let homePoints = 0;
 let guestPoints = 0;
+let homeScore = document.getElementById("home-points");
+let guestScore = document.getElementById("guest-points");
 
 //HOME POINTS//
 function onePointHome(){
     homePoints += 1;
-    document.getElementById("home-points").textContent = homePoints;
+    homeScore.textContent = homePoints;
 }
 
 function twoPointHome(){
     homePoints += 2;
-    document.getElementById("home-points").textContent = homePoints;
+    homeScore.textContent = homePoints;
 }
 
 function threePointHome(){
     homePoints += 3;
-    document.getElementById("home-points").textContent = homePoints;
+    homeScore.textContent = homePoints;
 }
 
 //GUEST POINTS//
 function onePointGuest(){
     guestPoints += 1;
-    document.getElementById("guest-points").textContent = guestPoints;
+    guestScore.textContent = guestPoints;
 }
 
 function twoPointGuest(){
     guestPoints += 2;
-    document.getElementById("guest-points").textContent = guestPoints;
+     guestScore.textContent = guestPoints;
 }
 
 function threePointGuest(){
     guestPoints += 3;
-    document.getElementById("guest-points").textContent = guestPoints;
+     guestScore.textContent = guestPoints;
 }
